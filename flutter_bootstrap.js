afba43516e23afe4b9c1c0d949bc082e11d57f8a
Ertan Fatih Albayın <fatihsnowden5@gmail.com>
2026-08-09 23:55:22 +0300

    website update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1354919199" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "311613894" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
